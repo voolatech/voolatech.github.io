4496862435e4a1320496116211842eeebee1aa6a
cuongtran3001 <cuongtran3001@gmail.com>
2017-01-17 23:49:37 +0700

    add vpaid

---
+++ LavaVpaid.js
@@ -0,0 +1,16 @@
+var LavaVpaid = function() {
+
+}
+
+LavaVpaid.prototype.configure = function(options) {
+	console.log('configure');
+}
+
+LavaVpaid.prototype.call = function(eventName, ...params) {
+	window.postMessage('{"eventName" : "' + eventName+ '", "params": "' + params.toString() + '"}', "http://voolatech.github.io/");
+	//window.postMessage('{"eventName" : "' + eventName+ '", "params": "' + params.toString() + '"}', "http://libs.lavanetwork.net");
+
+}
+
+window.LAVA = {};
+window.LAVA.VPAID = new LavaVpaid();
